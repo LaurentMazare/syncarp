b0b3da7f4a8a51c64d0dfe608254e71da7da8734
laurent <laurent.mazare@gmail.com>
2021-09-19 14:44:44 +0100

    Add the code for the ocaml example.

---
+++ examples/ocaml/rpc_client.ml
@@ -0,0 +1,91 @@
+open Core
+open Async
+
+let dispatch_exn rpc { Host_and_port.host; port } arg =
+  Rpc.Connection.with_client
+    (Tcp.Where_to_connect.of_host_and_port { host; port })
+    (fun conn -> Rpc.Rpc.dispatch_exn rpc conn arg)
+  >>| Result.ok_exn
+;;
+
+let pipe_dispatch_exn rpc { Host_and_port.host; port } arg f =
+  Rpc.Connection.with_client
+    (Tcp.Where_to_connect.of_host_and_port { host; port })
+    (fun conn ->
+       let%bind pipe, (_ : Rpc.Pipe_rpc.Metadata.t) =
+         Rpc.Pipe_rpc.dispatch_exn rpc conn arg
+       in
+       f pipe)
+  >>| Result.ok_exn
+;;
+
+let set_id_counter addr new_id = dispatch_exn Rpc_intf.set_id_counter addr new_id
+
+let set_id_counter_v0 addr new_id_pair =
+  dispatch_exn Rpc_intf.set_id_counter_v0 addr new_id_pair
+;;
+
+let get_unique_id addr =
+  let%map id = dispatch_exn Rpc_intf.get_unique_id addr () in
+  printf "UNIQUE ID: %d\n" id
+;;
+
+let counter_values addr =
+  pipe_dispatch_exn Rpc_intf.counter_values addr () (fun reader ->
+    Pipe.iter_without_pushback reader ~f:(fun i -> printf "COUNTER: %d\n%!" i))
+;;
+
+(* Setting up the command-line interface *)
+
+let host_and_port_param =
+  let open Command.Param in
+  flag
+    "-host-and-port"
+    (optional_with_default
+       { Host_and_port.host = "127.0.0.1"; port = 8080 }
+       host_and_port)
+    ~doc:"HOST:PORT server host and port"
+;;
+
+let get_unique_id_cmd =
+  Command.async
+    ~summary:"get unique id from server"
+    (let%map.Command host_and_port = host_and_port_param in
+     fun () -> get_unique_id host_and_port)
+;;
+
+let set_id_counter_cmd =
+  Command.async
+    ~summary:"forcibly set the unique id counter.  DANGEROUS"
+    (let%map_open.Command host_and_port = host_and_port_param
+     and i = anon ("counter" %: int) in
+     fun () -> set_id_counter host_and_port i)
+;;
+
+(* This one is actually unsupported by the server, so using it will trigger an error. *)
+let set_id_counter_cmd_v0 =
+  Command.async
+    ~summary:"forcibly set the unique id counter.  DANGEROUS"
+    (let%map_open.Command host_and_port = host_and_port_param
+     and id1 = anon ("counter1" %: int)
+     and id2 = anon ("counter2" %: int) in
+     fun () -> set_id_counter_v0 host_and_port (id1, id2))
+;;
+
+let counter_values_cmd =
+  Command.async
+    ~summary:"subscribe to changes to counter id"
+    (let%map.Command host_and_port = host_and_port_param in
+     fun () -> counter_values host_and_port)
+;;
+
+let () =
+  Command.run
+    (Command.group
+       ~summary:"Client for trivial Async-RPC server"
+       [ "get-unique-id", get_unique_id_cmd
+       ; "set-id-counter", set_id_counter_cmd
+       ; "set-id-counter-v0", set_id_counter_cmd_v0
+       ; "counter-values", counter_values_cmd
+       ])
+;;
---
+++ examples/ocaml/rpc_server.ml
@@ -0,0 +1,88 @@
+open Core
+open Async
+
+(* The list of implementations supported by the server.  The server state is simply a
+   counter used for allocating unique ids. *)
+let implementations =
+  [ Rpc.Rpc.implement Rpc_intf.get_unique_id (fun ctr () ->
+      printf ".%!";
+      incr ctr;
+      return !ctr)
+  ; Rpc.Rpc.implement Rpc_intf.set_id_counter (fun ctr i ->
+      printf "!%!";
+      if i = 0 then failwith "Can't set counter back to zero";
+      return (ctr := i))
+  ; Rpc.Pipe_rpc.implement Rpc_intf.counter_values (fun ctr () ->
+      let r, w = Pipe.create () in
+      let last_value = ref !ctr in
+      let send () =
+        last_value := !ctr;
+        Pipe.write w !ctr
+      in
+      don't_wait_for (send ());
+      Clock.every' ~stop:(Pipe.closed w) (sec 0.1) (fun () ->
+        if !last_value <> !ctr then send () else return ());
+      return (Ok r))
+  ]
+;;
+
+let main ~port =
+  Stdio.printf "Running on port %d\n%!" port;
+  let counter = ref 0 in
+  let log_unknown _counter ~rpc_tag ~version =
+      Stdio.printf "Unknown rpc: %s %d\n%!" rpc_tag version;
+      `Close_connection
+  in
+  let implementations =
+    Rpc.Implementations.create ~implementations ~on_unknown_rpc:(`Call log_unknown)
+  in
+  let log_error address exn =
+    Stdio.printf "Error %s: %s\n%!" (Socket.Address.to_string address) (Exn.to_string exn)
+  in
+  let log_handshake_error exn =
+      Stdio.printf "Error handshake: %s\n%!" (Exn.to_string exn);
+      Deferred.unit
+  in
+  match implementations with
+  | Error (`Duplicate_implementations (_ : Rpc.Description.t list)) -> assert false
+  | Ok implementations ->
+    let server =
+      Tcp.Server.create
+        (Tcp.Where_to_listen.of_port port)
+        ~on_handler_error:(`Call log_error)
+        (fun (_ : Socket.Address.Inet.t) reader writer ->
+          let heartbeat_config = Rpc.Connection.Heartbeat_config.create ~timeout:(Time_ns.Span.of_sec 5.) () in
+           Rpc.Connection.server_with_close
+             reader
+             writer
+             ~implementations
+             ~heartbeat_config
+             ~connection_state:(fun (_ : Rpc.Connection.t) -> counter)
+             ~on_handshake_error:(`Call log_handshake_error))
+    in
+    ignore (server : (Socket.Address.Inet.t, int) Tcp.Server.t Deferred.t);
+    never ()
+;;
+
+type t = int [@@deriving bin_io]
+type u = Int64.t [@@deriving bin_io]
+type v = t [@@deriving bin_io]
+type w = {t: int} [@@deriving bin_io]
+
+let print_shape s =
+  Stdio.printf "%s\n%!" (Bin_prot.Shape.eval s |> Bin_prot.Shape.Canonical.sexp_of_t |> Sexp.to_string);
+  Stdio.printf "%s\n%!" (Bin_prot.Shape.eval_to_digest s|> Bin_prot.Shape.Digest.to_hex)
+
+let () =
+  print_shape bin_shape_t;
+  print_shape bin_shape_u;
+  print_shape bin_shape_v;
+  print_shape bin_shape_w;
+  Command.async
+    ~summary:"A trivial Async-RPC server"
+    (let%map_open.Command port =
+       flag "-port" ~doc:" Port to listen on" (optional_with_default 8080 int)
+     in
+     fun () -> main ~port)
+  |> Command.run
+;;
